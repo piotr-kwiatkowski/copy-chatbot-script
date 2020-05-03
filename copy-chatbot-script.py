e6e353df3b01189bf94e6da0b15cb6a62abde8b7
Piotr Kwiatkowski <piotrkwiatkowski1@gmail.com>
2020-05-03 12:37:26 +0200

    Update formatting

--- copy-chatbot-script.py
+++ copy-chatbot-script.py
@@ -5,63 +5,61 @@ import re
 
 
 def usage():
-  print("""
-  Usage: copy-chatbot-script [REQUEST FILE] [API TOKEN] [PROJECT PUBLIC KEY]
-  """)
+    print("""
+  Usage: """, sys.argv[0], """[REQUEST FILE] [API TOKEN] [PROJECT PUBLIC KEY]""")
 
 
 def load_request_from_file(file):
-  f = open(file, encoding="utf8")
-  if f.mode == "r":
-    global request_to_process
-    request_to_process = f.read()
-    f.close()
-  # print("request_to_process after loading:", request_to_process)
-  # print("file_content:\n", file_content)
-  else:
-    print("Could not read this file :(\nExiting...")
-    sys.exit()
+    with open(file, encoding="utf8") as f:
+        global request_to_process
+        request_to_process = f.read()
+    # f = open(file, encoding="utf8")
+    # if f.mode == "r":
+    #   global request_to_process
+    #   request_to_process = f.read()
+    #   f.close()
+    # else:
+    #   print("Could not read this file :(\nExiting...")
+    #   sys.exit()
 
 
 def replace_dbNodeId_nr_with_null(request):
-  pattern = "(dbNodeId\\\"):(.*?),(\\\"id)"  # FIXME
-  regex_pattern = "^[0-9a-f]{32}$"
+    pattern = "(dbNodeId\\\"):(.*?),(\\\"id)"  # FIXME
+    regex_pattern = "^[0-9a-f]{32}$"
 
-  # print(request.count(pattern))
-  print("Number of instances:")
-  print("-- dbNodeId:", request.count("dbNodeId\\\":"))
-  print("-- api_token:", request.count("api_token\\\":\\\""))
-  print("-- project_public_key:", request.count("project_public_key"))
+    # print(request.count(pattern))
+    print("Number of instances:")
+    print("-- dbNodeId:", request.count("dbNodeId\\\":"))
+    print("-- api_token:", request.count("api_token\\\":\\\""))
+    print("-- project_public_key:", request.count("project_public_key"))
 
 
 def main():
-  if sys.version_info < (3, 6):
-    sys.exit("Python 3.6 required")
+    if sys.version_info < (3, 6):
+        sys.exit("Python 3.6 required")
 
-  try:
-    print("Processing file:", sys.argv[1])
-  except IndexError:
-    usage()
-    sys.exit(1)
+    try:
+        print("Processing file:", sys.argv[1])
+    except IndexError:
+        usage()
+        sys.exit(1)
 
-  global request_to_process
+    global request_to_process
 
-  # start = request.find("dbNodeId\\\":") + len("dbNodeId\\\":")
-  # end = request.find("dbNodeId\\\":")
-  # substring = request[start:end]
-  # print(substring)
+    # start = request.find("dbNodeId\\\":") + len("dbNodeId\\\":")
+    # end = request.find("dbNodeId\\\":")
+    # substring = request[start:end]
+    # print(substring)
 
-  # for item in request:
-  #   substring = re.search(pattern, request).group(1)
-  #   print(substring)
+    # for item in request:
+    #   substring = re.search(pattern, request).group(1)
+    #   print(substring)
 
+    # print(request.split("dbNodeId", 5)[1])
 
-  # print(request.split("dbNodeId", 5)[1])
-
-
-  load_request_from_file(sys.argv[1])
-  replace_dbNodeId_nr_with_null(request_to_process)
+    load_request_from_file(sys.argv[1])
+    replace_dbNodeId_nr_with_null(request_to_process)
 
 
 if __name__ == "__main__":
-  main()
+    main()
